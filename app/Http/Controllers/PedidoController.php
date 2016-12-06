c393f0a2465fc5073d6b20bba0bf918683affc67
Gary Robles <garymarcelorobles@gmail.com>
2016-12-05 20:20:47 -0400

    Vista de solicitud de pedido

--- app/Http/Controllers/PedidoController.php
+++ app/Http/Controllers/PedidoController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Cliente;
 use App\DetallePedido;
+use App\Pedido;
 use App\Producto;
 use Laracasts\Flash\Flash;
 
@@ -21,7 +22,9 @@ class PedidoController extends Controller
 //        $cliente = Cliente::where ('ESTADO',1)->orderBy ('NOMBRE','ASC')->paginate(5);
         $products = Producto::where('ESTADO',1)->orderBy ('ID','ASC')->paginate(5);
       //  print_r($products);
-        return view('pedidos.index')->with('productos',$products);  //primero la vista y luego la variable
+        $pedido_id = -1;
+        $todosDetallePedido = DetallePedido::where('ID_PEDIDO',-5)->orderBy ('ID','ASC')->paginate(10);//Puede que lo use
+        return view('pedidos.index')->with('productos',$products)->with('pedido_id',$pedido_id)->with('todosDetallePedido',-1)->with('pedidoFinalizado',0)->with('tipoOperacion',0)->with('totalPedido',0);  //primero la vista y luego la variable
     }
 
     /**
@@ -35,36 +38,144 @@ class PedidoController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Guarda un detalle pedido temporalmente
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
+        $tipoOperacion = $request->input('tipoOperacion');
+        if($tipoOperacion == 10){
+            
+            // print_r($request);//Imprimiendo lo que me llega
+             //Obteniendo un valor del form
+             $idPedido = $request->input('pedido_id');//obteneiendo la cantidad del producto
+
+             if($idPedido == -1){
+                 Flash::warning('Estimado cliente, adicione productos para ordenar.');  
+             }else{
+                 $pedido = Pedido::find($idPedido);
+                 $pedido->ESTADO = 1;
+                 $todosDetallePedido = DetallePedido::where('ID_PEDIDO',$idPedido)->orderBy ('ID','ASC')->paginate(10);
+                 $total = 0;
+                 foreach ($todosDetallePedido as $detallePedido){
+                     $total = $total + $detallePedido->SUBTOTAL;
+                 }
+                 $pedido->TOTAL = $total;
+                 $pedido->ID_MESA = 300;
+                 $pedido->save();
+             }
+             $products = Producto::where('ESTADO',1)->orderBy ('ID','ASC')->paginate(5);
+
+
+             return view('pedidos.index')->with('productos',$products)->with('pedido_id',$idPedido)->with('todosDetallePedido',-1)->with('pedidoFinalizado',1)->with('totalPedido',0);  //primero la vista y luego la variable
+            
+            
+        }else{
+
+
+       // print_r($request);//Imprimiendo lo que me llega
+        //Obteniendo un valor del form
+        $cantidad = $request->input('cantidad');//obteneiendo la cantidad del producto
+        $idProducto = $request->input('producto_id');//obteneiendo la cantidad del producto
+        $idPedido = $request->input('pedido_id');//obteneiendo la cantidad del producto
+         $importe = $request->input('importe');
+        $pedido;
+        if ($idPedido == -1){
+            $pedido = new Pedido();
+            $pedido->ESTADO=2;
+            $pedido->FECHA_PEDIDO=date("Y-m-d h:i:s");
+            $pedido->ID_CLIENTE=1;
+            $pedido->ID_MESA=300;
+            $pedido->TOTAL=0;
+            $pedido->save();            
+        }else{
+            $pedido = Pedido::find($idPedido);
+        }
+
+        
+        $detallePedido=new DetallePedido();
+        $detallePedido->CANTIDAD =$cantidad;
+        $detallePedido->IMPORTE =$importe;
+        $detallePedido->SUBTOTAL =$importe* $cantidad;
+        $detallePedido->ID_PRODUCTO =$idProducto;
+        $detallePedido->ID_PEDIDO =$pedido->ID;
+        $detallePedido->save();
+        
+  
+        $products = Producto::where('ESTADO',1)->orderBy ('ID','ASC')->paginate(10);
+        
+        $todosDetallePedido = DetallePedido::where('ID_PEDIDO',$pedido->ID)->orderBy ('ID','ASC')->paginate(10);
+        
+        $total=0;
+                 foreach ($todosDetallePedido as $detallePedido){
+                     $total = $total + $detallePedido->SUBTOTAL;
+                 }
+        
+
+        return view('pedidos.index')->with('productos',$products)->with('pedido_id',$pedido->ID)->with('todosDetallePedido',$todosDetallePedido)->with('pedidoFinalizado',0)->with('tipoOperacion',0)->with('totalPedido',$total);  //primero la vista y luego la variable
+            
+
+
+            
+        }
+        
+        
+    }
+
+
+
+    public function update(Request $request)
+    {
+       // print_r($request);//Imprimiendo lo que me llega
+        //Obteniendo un valor del form
+        $idPedido = $request->input('pedido_id');//obteneiendo la cantidad del producto
+        
+        if($idPedido == -1){
+            Flash::warning('Estimado cliente, adicione productos para ordenar.');  
+        }else{
+            $pedido = Pedido::find($idPedido);
+            $pedido->ESTADO = 1;
+            $todosDetallePedido = DetallePedido::where('ID_PEDIDO',$idPedido)->orderBy ('ID','ASC')->paginate(10);
+            $total = 0;
+            foreach ($todosDetallePedido as $detallePedido){
+                $total = $total + $detallePedido->SUBTOTAL;
+            }
+            $pedido->TOTAL = $total;
+            $pedido->ID_MESA = 300;
+            $pedido->save();
+        }
+        $products = Producto::where('ESTADO',1)->orderBy ('ID','ASC')->paginate(5);
+
+        
+        return view('pedidos.index')->with('productos',$products)->with('pedido_id',-1)->with('todosDetallePedido',-1)->with('pedidoFinalizado',1);  //primero la vista y luego la variable
+    }
+
+
+
+    
+
+    public function resumen(Request $request)
+    {
         print_r($request);
 
-        $detalle = new DetallePedido( $request ->all());       
-        // $cliente->ci = $request->numero;
-        // $cliente->nombre = $request->nombre;
-        // $cliente->telefono = $request->telefono;
-        // $cliente->correo = $request->email;
-        $detalle->save();
-       // dd ('Cliente Creado Satisfactoriamente!!');
-        Flash::success("Se ha guardado temporalmente el pedido " . $cliente-> NOMBRE . " de forma Exitosa !");
         return redirect()->route('clientes.index');
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
+    }    
+    
+//    /**
+//     * Display the specified resource.
+//     *
+//     * @param  int  $id
+//     * @return \Illuminate\Http\Response
+//     */
+//    public function mostrarDetalle($idPedido)
+//    {
+//        
+//        $detallePedidos = DetallePedido::where('ID_PEDIDO',$idPedido)->orderBy ('ID','ASC')->paginate(10);
+//        return view('pedidos.detallePedido')->with('detallePedidos',$detallePedidos); 
+//        //
+//    }
 
     /**
      * Show the form for editing the specified resource.
@@ -79,23 +190,23 @@ class PedidoController extends Controller
         return view ('clientes.edit')-> with ('clientes',$cliente);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $ID)
-    {
-        //
-        $cliente = new Cliente();
-        $cliente=$cliente->find($ID);
-        $cliente->fill($request->all());
-        $cliente->save();
-        Flash::warning('El cliente '. $cliente->NOMBRE. ' ha sido editado exitosamente..');
-        return redirect()->route('clientes.index');
-    }
+//    /**
+//     * Update the specified resource in storage.
+//     *
+//     * @param  \Illuminate\Http\Request  $request
+//     * @param  int  $id
+//     * @return \Illuminate\Http\Response
+//     */
+//    public function update(Request $request, $ID)
+//    {
+//        //
+//        $cliente = new Cliente();
+//        $cliente=$cliente->find($ID);
+//        $cliente->fill($request->all());
+//        $cliente->save();
+//        Flash::warning('El cliente '. $cliente->NOMBRE. ' ha sido editado exitosamente..');
+//        return redirect()->route('clientes.index');
+//    }
 
     /**
      * Remove the specified resource from storage.
--- resources/views/pedidos/index.blade.php
+++ resources/views/pedidos/index.blade.php
@@ -1,10 +1,15 @@
+
 @extends('pedidos.layouts.site')
 @section('title', 'Inicio')
 @section('content')
+
+
+  @if ($pedidoFinalizado=== 0)
+
 <div class="list-platos col-xs-12">
     <ul class="list-group">
-        
-      @foreach ($productos as $producto)
+
+        @foreach ($productos as $producto)
         <li class="row items-platos">
             <div class="col-xs-3">
                 <img src="{{asset('imagenes/productos/'.$producto->RUTA_IMAGEN)}}" class="image-item">   
@@ -16,59 +21,171 @@
                 </div>
             </div>
             <div class="col-xs-2">
-                <div class="row detallar text-center" data-toggle="modal" data-target="#myModal">
+                <div class="row detallar text-center" data-toggle="modal" data-target="#myModal{{$producto->ID}}">
                     +
                 </div>
             </div>
         </li>
 
-      @endforeach 
+        @endforeach 
     </ul>
 </div>
 <!-- Modal -->
-<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+@foreach ($productos as $producto)
+<div class="modal fade" id="myModal{{$producto->ID}}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header-laravel">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title" id="myModalLabel">Titulo del Producto</h4>
+                <h4 class="modal-title" id="myModalLabel">{{$producto->NOMBRE}}</h4>
             </div>
             <div class="modal-body-laravel">
-                <img src="img/platocomidauno.jpg" class="image-item"> 
+                <img src="{{asset('imagenes/productos/'.$producto->RUTA_IMAGEN)}}" class="image-item"> 
                 <div class="form-adicionar">
-                    <form >
-                        <span class="col-xs-3">Cantidad</span> 
-                        <div class="col-xs-5">
-                            <div id="1" class="row input-group input-group-option quantity-wrapper">
+                    {!! Form::open(['route' =>'pedidos.store','method' => 'POST'])!!}
+                    <span class="col-xs-3">Cantidad</span> 
+                    <div class="col-xs-5">
+                        <div id="1" class="row input-group input-group-option quantity-wrapper">
 
-                                <span  class="input-group-addon input-group-addon-remove quantity-remove btn">
-                                    <span class="glyphicon glyphicon-minus"></span>
-                                </span>
+                            <span  class="input-group-addon input-group-addon-remove quantity-remove btn">
+                                <span class="glyphicon glyphicon-minus"></span>
+                            </span>
+                                                                                   
+                            @if ($pedido_id === -1)
+                                {{ Form::hidden('pedido_id', -1) }}
+                            @else
+                                {{ Form::hidden('pedido_id', $pedido_id) }}
+                            @endif                            
+                            
+                            {{ Form::hidden('tipoOperacion', 20) }}
+                            
+                            {{ Form::hidden('producto_id', $producto->ID) }}
+                        
+                            <input  id="cantidad" type="text" value="6" name="cantidad" class="form-control quantity-count" placeholder="1">
 
-                                <input  id="1inp" type="text" value="6" name="option[]" class="form-control quantity-count" placeholder="1">
-
-                                <span class="input-group-addon input-group-addon-remove quantity-add btn">
-                                    <span class="glyphicon glyphicon-plus"></span>
-                                </span>
-
-                            </div>
+                            <span class="input-group-addon input-group-addon-remove quantity-add btn">
+                                <span class="glyphicon glyphicon-plus"></span>
+                            </span>
+                            <input  id="precio" type="hidden" value="{{$producto->PRECIO}}" name="importe" >
+                          
                         </div>
-                        <div class="col-xs-4">
-                            <button type="button" class="btn btn-primary" data-dismiss="modal">Adicionar</button>
-                        </div>
-                    </form>
+                    </div>
+                    <div class="col-xs-4">                        
+                          {!! Form::submit('Adicionar',['class' => 'btn btn-primary']) !!}
+                    </div>
+                    {!! Form::close() !!}
                 </div>
                 <div class="modal-body">
-                    <h3>Titulo de Producto</h3>
-                    <p>Descripcion del producto. Descripcion del producto. Descripcion del producto.</p>
+                    <h3>Description</h3>
+                    <p>{{$producto->DESCRIPCION}}</p>
                 </div>
             </div>
         </div>
     </div>
 </div>
+@endforeach 
 
-<div id="btn-pedido" class="col-xs-12 text-center">
+<div id="btn-pedido" class="col-xs-12 text-center" data-toggle="modal" data-target="#finalizar" >
     <h3>Realizar Pedido</h3>
 </div>
+
+
+<div class="modal fade" id="finalizar" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="myModalLabel">Finalizar Pedido</h4>
+            </div>
+            <div class="modal-body">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Item</th>
+                            <th>Cantidad</th>
+                            <th>Subtotal</th>
+                        </tr>
+                    </thead>
+                    
+                    
+                    
+                    
+<!-- Hola edson seria que en tu logica lo crees con este if, la primera vez que ingresen a la vista estara sin registros, pero conforme se ingresen productos se ira actualizando la variable-->                    
+                            @if ($pedido_id === -1)
+                                {{ Form::hidden('pedido_id', -1) }}
+                            @else
+                            <tbody>            
+                     @foreach ($todosDetallePedido as $detalle)
+                    
+
+                        <tr>
+                            <td>{{$detalle->producto->NOMBRE}}</td>
+                            <td class="text-center">{{$detalle->IMPORTE}} * {{$detalle->CANTIDAD}}</td>
+                            <td>{{$detalle->SUBTOTAL}}</td>
+                        </tr>
+                        
+
+                   
+                    @endforeach 
+                    
+                    
+                    
+                    
+                     </tbody>
+                                {{ Form::hidden('pedido_id', $pedido_id) }}
+                            @endif   
+                    
+                    
+        
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                   
+                </table>
+                <div class="row">
+                <div class="col-xs-12 text-center" style="background: #ccc;margin-bottom: 20px;">
+                    Total<br>
+                  {{$totalPedido}}
+
+                </div>
+                <div class="col-xs-6" style="text-align: right;">
+                <button type="button" class="btn btn-primary align-right" data-dismiss="modal">Cancelar</button>
+               </div>
+                <div class="col-xs-6">                        
+                   
+               @if ($pedido_id === -1)
+                           @else
+                    {!! Form::open(['route' =>'pedidos.store','method' => 'POST'])!!}
+                                        
+                    {{ Form::hidden('pedido_id', $pedido_id) }}
+                    {{ Form::hidden('tipoOperacion', 10) }}
+                    
+                    
+                          {!! Form::submit('Confirmar',['class' => 'btn btn-primary']) !!}
+                    
+                  
+
+                  {!! Form::close() !!}
+                        @endif   
+  </div>
+            </div>
+                                    
+            </div>
+        </div>
+    </div>
+</div>
+
+ @else
+<div class="list-platos col-xs-12 textcenter">
+    <h3>Gracias por realizar tu pedido. Tu  numero pedido es {{$pedido_id}}</h3>
+    </div>
+  @endif  
 
 @endsection
